08103272eb95240565698d8dff201b31f009b482
Ryan Weald <ryan@weald.com>
2022-05-19 14:06:19 -0700

    Add docker setup for solana test validator
    1st pass at running a solana test validator node in docker. In theory it
    shoud clone down our existing accounts + programs for cannonical swap
    and TBCs, but I'm still working on testing that.

---
+++ solana-test-validator-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+solana-test-validator \
+  --url devnet \
+  --clone TBCwReYDDw8SvwVVKJHgatzeXKrLHnaTPyDGwkUoBsq \
+  --clone CSwAp3hdedZJBmhWMjv8BJ7anTLMQ2hBqKdnXV5bB3Nz \
+  --clone TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA \
+  --quiet \
+  -r
