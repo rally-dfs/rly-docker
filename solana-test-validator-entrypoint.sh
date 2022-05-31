c57b751c78d0b06f089234f707d96b6cce67a172
Ryan Weald <ryan@weald.com>
2022-05-31 12:10:44 -0700

    Clone missing canonical swap executable data account
    Need to explicitly clone this account that contains the canonical swap
    executable data like we did for the TCB contract.

--- solana-test-validator-entrypoint.sh
+++ solana-test-validator-entrypoint.sh
@@ -6,5 +6,6 @@ solana-test-validator \
   --clone CSwAp3hdedZJBmhWMjv8BJ7anTLMQ2hBqKdnXV5bB3Nz \
   --clone TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA \
   --clone 81CFqcdCmhQY1YSupLfxvJ9XB8erXzf2jB59AhZa548g \
+  --clone 4XaoFcHfJHGdwxsj5vKDLxL9aSQmLXSFrDvCx2LMeaMe \
   --quiet \
   -r
